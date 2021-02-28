1d42dc6588eea6c85a47afe36ed6e4304c466051
Андрей Коленков <avoknelok@mail.ru>
2021-02-28 20:35:45 +0300

    хотфикс

--- BMSTUCraft Launcher/GameLauncher.cs
+++ BMSTUCraft Launcher/GameLauncher.cs
@@ -9,7 +9,7 @@ namespace BMSTUCraft_Launcher
 {
     public class GameLauncher
     {
-        public int maxMem = 2048;
+        public int maxMem = 3072;
         public string nickName = "Steve";
 
         #region launchSettings
