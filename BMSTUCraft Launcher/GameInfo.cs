b1d2e75153f0090d8623aa4689c898c1de57263e
Андрей Коленков <avoknelok@mail.ru>
2021-03-01 16:49:30 +0300

    Обновлена сборка модов

--- BMSTUCraft Launcher/GameInfo.cs
+++ BMSTUCraft Launcher/GameInfo.cs
@@ -5,7 +5,7 @@ namespace BMSTUCraft_Launcher
 {
     internal static class GameInfo
     {
-        public static string url = "https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/MGRdInP8JBfFiQ";
+        public static string url = "https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/NjvHRe0EKEHOKg";
         public static string roamingFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
         public static string minecraftFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/.bmstucraft";
         public static string cfgPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/.bmstucraft/launcheroptions.txt";
