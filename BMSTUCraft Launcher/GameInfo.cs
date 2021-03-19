c266210acd4b031c6b5b34e55eb825c763b33930
Андрей Коленков <avoknelok@mail.ru>
2021-03-19 17:10:19 +0300

    Я даун держу в курсе

--- BMSTUCraft Launcher/GameInfo.cs
+++ BMSTUCraft Launcher/GameInfo.cs
@@ -5,7 +5,7 @@ namespace BMSTUCraft_Launcher
 {
     internal static class GameInfo
     {
-        public static string url = "https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/0HK2xkv5CvfThg";
+        public static string url = "https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/EaE_FOpikzVdjQ";
         public static string roamingFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
         public static string minecraftFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/.bmstucraft";
         public static string cfgPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/.bmstucraft/launcheroptions.txt";
